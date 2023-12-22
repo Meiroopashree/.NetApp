68fd52db8f1c0fa2118269a3afa66918b878284e
Iamneo <techadmin@iamneo.ai>
2023-12-22 05:22:41 +0000

    Auto-save-commit-12/22/2023--10:52:46-AM

--- dotnetapp/Models/Book.cs
+++ dotnetapp/Models/Book.cs
@@ -3,8 +3,9 @@ namespace dotnetapp.Models
     public class Book
     {
         public int Id{get; set;}
-        public string Name{get; set;}
+        public string Name {get; set;}
         public string Author{get;set;}
-        public string 
+        public string language{get; set;}
+        public string type{get;set;}
     }
 }
