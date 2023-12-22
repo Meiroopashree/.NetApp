d04e9ea9468a9fd59a939c54dd7f9b00a53ae8c2
Iamneo <techadmin@iamneo.ai>
2023-12-22 11:38:03 +0000

    Manual-save-commit-12/22/2023--5:08:09-PM

--- dotnetapp/Controllers/BookController.cs
+++ dotnetapp/Controllers/BookController.cs
@@ -8,7 +8,6 @@ namespace dotnetapp.Controllers
 {
     public class BookController : Controller
     {
-        // private string connectionString = "Your_Connection_String_Here";
         private string connectionString = "User ID=sa;password=examlyMssql@123;server=bfdeeddcedfabcfacbdcbaeadbebabcdebdca-0;Database=CRUDOperations;trusted_connection=false;Persist Security Info=False;Encrypt=False";
 
         public IActionResult Create()
