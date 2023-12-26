f80f4dc457830ef1085db22f168f5c9caa21ba3f
Iamneo <techadmin@iamneo.ai>
2023-12-26 07:06:31 +0000

    Auto-save-commit-12/26/2023--12:36:37-PM

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -1,3 +1,18 @@
+using dotnetapp.Models;
+using System;
+using NUnit.Framework;
+using System.Reflection;
+
+using NUnit.Framework;
+using System;
+using System.Reflection;
+using System.Linq;
+using Deliveryboy.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Deliveryboy.Models;
+ 
+namespace TestProject;
 namespace TestProject;
 
 public class Tests
@@ -12,4 +27,16 @@ public class Tests
     {
         Assert.Pass();
     }
+    // [Test]
+    //     public void BookController_ClassExists()
+    //     {
+    //         string assemblyName = "dotnetapp";
+    //         string typeName = "dotnetapp.Controllers.BookController";
+    //         Assembly assembly = Assembly.Load(assemblyName);
+    //         Type controllerType = assembly.GetType(typeName);
+    //         Assert.IsNotNull(controllerType);
+    //         var controller = Activator.CreateInstance(controllerType);
+    //         Assert.IsNotNull(controller);
+    //     }
+    
 }
