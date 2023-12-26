ba08c98472c596d0c95e1117f8d9f66fcdac4f95
Iamneo <techadmin@iamneo.ai>
2023-12-26 09:40:51 +0000

    Start-project-save-commit-12/26/2023--3:10:55-PM

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -18,18 +18,6 @@ public class Tests
         Assert.Pass();
     }
 
-    // [Test]
-    //     public void BookController_ClassExists()
-    //     {
-    //         string assemblyName = "dotnetapp";
-    //         string typeName = "dotnetapp.Controllers.BookController";
-    //         Assembly assembly = Assembly.Load(assemblyName);
-    //         Type controllerType = assembly.GetType(typeName);
-    //         Assert.IsNotNull(controllerType);
-    //         var controller = Activator.CreateInstance(controllerType);
-    //         Assert.IsNotNull(controller);
-    //     }
-
     [Test]
         public void Book_ClassExists()
         {
@@ -41,5 +29,65 @@ public class Tests
             var ride = Activator.CreateInstance(rideType);
             Assert.IsNotNull(ride);
         }
-    
+
+    [Test]
+        public void BookController_ClassExists()
+        {
+            string assemblyName = "dotnetapp";
+            string typeName = "dotnetapp.Controllers.BookController";
+            Assembly assembly = Assembly.Load(assemblyName);
+            Type controllerType = assembly.GetType(typeName);
+            Assert.IsNotNull(controllerType);
+            var controller = Activator.CreateInstance(controllerType);
+            Assert.IsNotNull(controller);
+        }
+        [Test]
+            public void BookController_CreateMethodExists()
+            {
+                string assemblyName = "dotnetapp";
+                string typeName = "dotnetapp.Controllers.BookController";
+                Assembly assembly = Assembly.Load(assemblyName);
+                Type controllerType = assembly.GetType(typeName);
+
+                Type[] parameterTypes = new Type[] { typeof(Book) };
+
+                MethodInfo createMethod = controllerType.GetMethod("Create", parameterTypes);
+                Assert.IsNotNull(createMethod);
+            }
+            [Test]
+            public void BookController_UpdateMethodExists()
+            {
+                string assemblyName = "dotnetapp";
+                string typeName = "dotnetapp.Controllers.BookController";
+                Assembly assembly = Assembly.Load(assemblyName);
+                Type controllerType = assembly.GetType(typeName);
+
+                Type[] parameterTypes = new Type[] { typeof(Book) };
+
+                MethodInfo updateMethod = controllerType.GetMethod("Update", parameterTypes);
+                Assert.IsNotNull(updateMethod);
+            }
+            [Test]
+            public void BookController_ReadMethodExists()
+            {
+                string assemblyName = "dotnetapp";
+                string typeName = "dotnetapp.Controllers.BookController";
+                Assembly assembly = Assembly.Load(assemblyName);
+                Type controllerType = assembly.GetType(typeName);
+            
+                MethodInfo readMethod = controllerType.GetMethod("Index");
+                Assert.IsNotNull(readMethod);
+            }
+            [Test]
+            public void BookController_DeleteMethodExists()
+            {
+                string assemblyName = "dotnetapp";
+                string typeName = "dotnetapp.Controllers.BookController";
+                Assembly assembly = Assembly.Load(assemblyName);
+                Type controllerType = assembly.GetType(typeName);
+                
+                MethodInfo deleteMethod = controllerType.GetMethod("Delete");
+                Assert.IsNotNull(deleteMethod);
+            }
+  
 }
