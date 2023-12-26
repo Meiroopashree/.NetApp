b7973fefd3ed3bf42365fc64fe5e6cc9134d6578
Iamneo <techadmin@iamneo.ai>
2023-12-26 04:48:05 +0000

    Auto-save-commit-12/26/2023--10:18:12-AM

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -1,15 +1,28 @@
-namespace TestProject;
+using System;
+// using dotnetapp.Controllers;
+using NUnit.Framework;
+using System.Reflection;
+using dotnetapp.Models;
 
-public class Tests
-{
-    [SetUp]
-    public void Setup()
-    {
-    }
+namespace TestProject  
 
-    [Test]
-    public void Test1()
+    public class Tests
     {
-        Assert.Pass();
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Book_ClassExists()
+        {
+            string assemblyName = "dotnetapp";
+            string typeName = "dotnetapp.Models.Book";
+            Assembly assembly = Assembly.Load(assemblyName);
+            Type bookType = assembly.GetType(typeName);
+            Assert.IsNotNull(bookType);
+            var book = Activator.CreateInstance(bookType);
+            Assert.IsNotNull(book);
+        }
     }
 }
